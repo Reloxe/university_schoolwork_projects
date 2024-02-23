7b4d1f937ef57c03af5cab7966a7d2d737cdf9a0
Reloxe <97050615+Reloxe@users.noreply.github.com>
2024-02-24 00:29:08 +0300

    Add files via upload

---
+++ calculator_hesapmakinesi_2-template/template1/hesapmakinesi/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesapmakinesi
+{
+    internal class Program
+    {
+        static void toplama() {
+            Console.Write("İlk sayıyı girin-> ");
+            int ilksayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı girin-> ");
+            int ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sonuç-> " + (ilksayi + ikincisayi));
+        }
+
+        static void cikarma()
+        {
+            Console.Write("İlk sayıyı girin-> ");
+            int ilksayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı girin-> ");
+            int ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sonuç-> " + (ilksayi - ikincisayi));
+        }
+
+        static void carpma()
+        {
+            Console.Write("İlk sayıyı girin-> ");
+            int ilksayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı girin-> ");
+            int ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sonuç-> " + (ilksayi * ikincisayi));
+        }
+
+        static void bolme()
+        {
+            Console.Write("İlk sayıyı girin-> ");
+            int ilksayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("İkinci sayıyı girin-> ");
+            int ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sonuç-> " + (ilksayi / ikincisayi));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------ Hesap Makinesi ------");
+            Console.WriteLine("Toplama İşlemi yapmak için 1");
+            Console.WriteLine("Çıkarma İşlemi yapmak için 2");
+            Console.WriteLine("Çarpma İşlemi yapmak için 3");
+            Console.WriteLine("Bölme İşlemi yapmak için 4");
+
+            Console.Write("Seçiminizi yapınız: ");
+            string secim = Console.ReadLine();
+            switch (secim)
+            {
+                case "1":
+                    toplama();
+               break;
+                case "2":
+                    cikarma();
+               break; 
+                case "3":
+                    carpma();
+               break;
+                case "4":
+                    bolme();
+               break;
+                default:
+                    Console.WriteLine("Hatalı Seçim yaptınız !");
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ calculator_hesapmakinesi_2-template/template2/hesaplayici/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hesaplayici
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("# Hesap Makinesi #");
+            Console.WriteLine("Toplama İşlemi için | 1");
+            Console.WriteLine("Çıkarma İşlemi için | 2");
+            Console.WriteLine("Çarpma İşlemi için | 3");
+            Console.WriteLine("Bölme İşlemi için | 4");
+
+            Console.Write("Seçiminizi yapınız: ");
+            string secim = Console.ReadLine();
+            switch (secim)
+            {
+                case "1":
+                    Console.Write("İlk sayı:  ");
+                    int ilksayi = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("İkinci sayı:  ");
+                    int ikincisayi = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("İşlem sonucu: " + (ilksayi + ikincisayi));
+                    break;
+                case "2":
+                    Console.Write("İlk sayı:  ");
+                    int ilksayii = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("İkinci sayı:  ");
+                    int ikincisayii = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("İşlem sonucu: " + (ilksayii - ikincisayii));
+                    break;
+                case "3":
+                    Console.Write("İlk sayı:  ");
+                    int ilksayiii = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("İkinci sayı:  ");
+                    int ikincisayiii = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("İşlem sonucu: " + (ilksayiii * ikincisayiii));
+                    break;
+                case "4":
+                    Console.Write("İlk sayı:  ");
+                    int ilksayiiii = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("İkinci sayı:  ");
+                    int ikincisayiiii = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("İşlem sonucu: " + (ilksayiiii / ikincisayiiii));
+                    break;
+                default:
+                    Console.WriteLine("Hatalı Seçim");
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
